58ebab7766f4869ffd1b01640480a89dcf788370
Alvaro <alvarovk94@gmail.com>
2017-03-12 12:57:05 +0100

    Intento de creación Socket y procesamiento de la información.

--- src/main/java/apostada/Application.java
+++ src/main/java/apostada/Application.java
@@ -1,6 +1,13 @@
 package apostada;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.Executor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
@@ -8,13 +15,54 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import apostada.entidades.Apuesta;
+import apostada.entidades.Partido;
+import apostada.servicios.ApuestaService;
+import apostada.servicios.PartidoService;
+
+
+
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
 public class Application extends AsyncConfigurerSupport  {
 
-	public static void main(String[] args) {
+
+	@Autowired
+	private static PartidoService partidoService;
+
+	@Autowired
+	private static ApuestaService apuestaService;
+	
+	
+	public static void main(String[] args) throws ClassNotFoundException {
 		SpringApplication.run(Application.class, args);
+		
+		//El puerto de la página
+		int puerto = 8888;
+		try{
+			ServerSocket serverSocket = new ServerSocket(puerto);
+			Socket laapostada = serverSocket.accept();
+			ObjectInputStream in = new ObjectInputStream(laapostada.getInputStream());
+			ObjectOutputStream out = new ObjectOutputStream(laapostada.getOutputStream());
+	
+			while(true){
+				 Apuesta apuesta = (Apuesta)in.readObject();
+				 Partido partido = apuesta.getPartido();
+				 partido.ajusteCuota(apuesta.getCantidadApostada(), apuesta.getResultado());
+				 partidoService.save(partido);
+				 apuestaService.save(apuesta);
+				 out.writeObject(partido);
+			}
+			
+		
+		
+		}
+		
+		
+		catch(IOException e){
+			System.out.println("Fallo en la conexión");
+		}
 	}
 
 	@Override
